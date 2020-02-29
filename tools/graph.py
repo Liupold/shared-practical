ddb02a3713b5b4e095a19093de9747132606b136
liupold <rohn.ch@gmail.com>
2020-02-29 20:04:12 +0530

    graph script fixed

--- tools/graph.py
+++ tools/graph.py
@@ -1,44 +1,52 @@
-import sys
+import numpy as np
+from sys import argv
 import math
+import os
 
-try:
-    data_file = sys.argv[1]
-except IndexError:
-    data_file = 'my.data'
+########### Settings #############
+                                                            # defaults
+extend_fraction_x = 0.1 # fraction between [0..1]                0.1
+extend_fraction_y = 0.1 # fraction between [0..1]                0.1
+no_of_div_x = 160 # (small divisions in graph paper (x))         160
+no_of_div_y = 200 # (small divisions in graph paper (y))         200
 
-with open(data_file, 'r') as f:
-    data = f.read()
 
-pm_points = [pm_point.split(' ') for pm_point in data.split('\n')]
-points = [(eval(x), eval(y)) for x, y in pm_points]
+##################################
 
-x_list = [x for x, y in points]
-y_list = [y for x, y in points]
+file_name = argv[1] if len(argv) > 2 else 'data.dat'
+if not os.path.isfile(file_name): raise FileNotFoundError(file_name + 'not found')
 
-x_max = max(x_list)
-x_min = min(x_list)
+x_array, y_array = np.loadtxt(file_name).transpose()
 
-y_max = max(y_list)
-y_min = min(y_list)
+# calculate ranges
+max_fraction_x = 1 + extend_fraction_x
+min_fraction_x = 1 - extend_fraction_x
+max_fraction_y = 1 + extend_fraction_y
+min_fraction_y = 1 - extend_fraction_y
 
-x_range = x_max * 1.1, (x_min * 0.9 if x_min > 0 else x_min * (1.1))
-y_range = y_max * 1.1, (y_min * 0.9 if y_min > 0 else y_min * (1.1))
 
-del_x = x_range[0] - x_range[1]
-del_y = y_range[0] - y_range[1]
+x_max, x_min = max(x_array), min(x_array)
+y_max, y_min = max(y_array), min(y_array)
 
-no_of_div_x = 160
-no_of_div_y = 200
+x_range = (x_max * max_fraction_x if x_max > 0 else x_max * min_fraction_x), \
+        (x_min * min_fraction_x if x_min > 0 else x_min * max_fraction_x)
+y_range = (y_max * max_fraction_y if y_max > 0 else y_max * min_fraction_y),\
+        (y_min * min_fraction_y if y_min > 0 else y_min * max_fraction_y)
 
-scale_x = del_x / no_of_div_x
-scale_y = del_y / no_of_div_y
 
-origin = (x_range[1], y_range[1])
-print(f"Origin: {origin}\n\n")
+# calculate scale
+scale_x = (x_range[0] - x_range[1]) / no_of_div_x
+scale_y = (y_range[0] - y_range[1]) / no_of_div_y
 
-print(f"Scale x: {scale_x}")
-print(f"Scale y: { scale_y}\n\n")
+origin = x_range[1], y_range[1]
 
+
+# print basic info
+print(f"Origin: { ['%.2E' % i for i  in origin]}")
+print(f"Scale x: {'%.2E' % scale_x} <Units>")
+print(f"Scale y: {'%.2E' % scale_y} <Units>")
+
+print('\n\n')
 print("X axis points:")
 for i in range(1, math.ceil(no_of_div_x / 10) + 1):
     print(f"{i * 10} :{round(origin[0] + (i * 10) * scale_x, 2)}")
@@ -50,8 +58,7 @@ for i in range(1, math.ceil(no_of_div_y / 10) + 1):
 print('\n')
 
 
-for point in points:
-    x, y = point
+for x, y in zip(x_array, y_array):
     x_ = (x - origin[0]) / scale_x
     y_ = (y - origin[1]) / scale_y
-    print(f"Point({point}): ({round(x_)}, {round(y_)})")
+    print(f"Point{x, y}: [{int(round(x_))}, {int(round(y_))}]")
